7367c94f6a4cedde5f451cd000d55e6e51f16313
Mikkel konyher <mikkelkonyher@gmail.com>
2024-02-19 12:00:42 +0100

    19.02 12.00

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -6,7 +6,75 @@
 //Console.Write(" ");
 //Console.Write("You wrote your first lines of code.");
 
-Console.WriteLine("This is the first line.");
-Console.Write("This is ");
-Console.Write("the second ");
-Console.Write("line.");
+//Console.WriteLine("This is the first line.");
+//Console.Write("This is ");
+//Console.Write("the second ");
+//Console.Write("line.");
+
+//Console.WriteLine('b');
+//Console.WriteLine(123);
+//Console.WriteLine(0.25F);
+//Console.WriteLine(2.625);
+//Console.WriteLine(12.39816m);
+
+//Console.WriteLine(true);
+//Console.WriteLine(false);
+
+//string firstName;
+//firstName = "Bob";
+//Console.WriteLine(firstName);
+
+//firstName = "Bob";
+//Console.WriteLine(firstName);
+//firstName = "Liem";
+//Console.WriteLine(firstName);
+//firstName = "Isabella";
+//Console.WriteLine(firstName);
+//firstName = "Yasmin";
+//Console.WriteLine(firstName);
+
+//string firstName = "Bob";
+//Console.WriteLine(firstName);
+
+
+//var firstName = "Bob";
+//var messages = 3;
+//var temperature = 34.4m;
+
+//Console.Write("Hello, " + firstName + "! ");
+//Console.Write("You have " + messages + " messengers in your inbox. The temperature is " + temperature + " celcius.");
+
+//Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+//Console.WriteLine("Invoice: 1021\t\tComplete!");
+//Console.WriteLine("Invoice: 1022\t\tComplete!");
+//Console.WriteLine("\nOutput Directory:\t");
+
+//Console.Write(@"c:\invoices");
+
+//Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+//Console.WriteLine("Invoice: 1021\t\tComplete!");
+//Console.WriteLine("Invoice: 1022\t\tComplete!");
+//Console.WriteLine("\nOutput Directory:\t");
+//Console.Write(@"c:\invoices");
+
+//// To generate Japanese invoices:
+//// Nihon no seikyū-sho o seisei suru ni wa:
+//Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+//// User command to run an application
+//Console.WriteLine(@"c:\invoices\app.exe -j");
+
+//string firstName = "Bob";
+//string message = "Hello " + firstName;
+//Console.WriteLine(message);
+
+string firstName = "Bob";
+string greeting = "Hello";
+string message = greeting + " " + firstName + "!";
+Console.WriteLine(message);
+
+
+
+
+
+
+
