73fcf3c6d11194d968b731aba7df66bca2f24676
Mikkel konyher <mikkelkonyher@gmail.com>
2024-02-19 13:55:25 +0100

    19.02 13.55

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -87,23 +87,76 @@
 //string projectName = "First-Project";
 //Console.WriteLine($@"C:\Output\{projectName}\data");
 
-string projectName = "ACME";
-string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+//string projectName = "ACME";
+//string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 
 
-Console.WriteLine("View English Ouput: \n\t" + $@"C:\Excercise\{projectName}\data.txt" + "\n");
+//Console.WriteLine("View English Ouput: \n\t" + $@"C:\Excercise\{projectName}\data.txt" + "\n");
 
 
-Console.WriteLine(russianMessage + "\n\t" + $@"C:\Excercise\{projectName}\ru-RU\data.txt");
+//Console.WriteLine(russianMessage + "\n\t" + $@"C:\Excercise\{projectName}\ru-RU\data.txt");
 
 
+//string firstName = "Bob";
+//int widgetsSold = 7;
+//Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
 
+//int sum = 7 + 5;
+//int difference = 7 - 5;
+//int product = 7 * 5;
+//int quotient = 7 / 5;
 
+//Console.WriteLine("Sum: " + sum);
+//Console.WriteLine("Difference: " + difference);
+//Console.WriteLine("Product: " + product);
+//Console.WriteLine("Quotient: " + quotient);
 
+//decimal decimalQuotient = 7.0m / 5;
+//Console.WriteLine($"Decimal quotient: {decimalQuotient}");
 
+//int first = 7;
+//int second = 5;
+//decimal quotient = (decimal)first / (decimal)second;
+//Console.WriteLine(quotient);
 
+//Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+//Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
 
+//int value1 = 3 + 4 * 5;
+//int value2 = (3 + 4) * 5;
+//Console.WriteLine(value1);
+//Console.WriteLine(value2);
 
+//int value = 1;
 
+//value = value + 1;
+//Console.WriteLine("first increment: " + value);
+
+//value += 1;
+//Console.WriteLine("Second increment: " + value);
+
+//value++;
+//Console.WriteLine("Third increment: " + value);
+
+//value = value - 1;
+//Console.WriteLine("First Decrement: " + value);
+
+//value -= 1;
+//Console.WriteLine("Second decrement: " + value);
+
+//value--;
+//Console.WriteLine("Third decrement: " + value);
+
+//int value = 1;
+//value++;
+//Console.WriteLine("First: " + value);
+//Console.WriteLine($"Second: { value++}");
+//Console.WriteLine("Third: " + value);
+//Console.WriteLine("Fourth: " + (++value));
+
+int fahrenheit = 94;
+decimal convert = (fahrenheit - 32m) * (5m / 9m);
+
+Console.WriteLine("The temperature is " + convert + " celcius");
 
 
