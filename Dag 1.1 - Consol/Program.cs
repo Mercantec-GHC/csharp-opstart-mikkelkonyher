52627e2940182cd73402ff97b18f7a6f23f64569
Mikkel konyher <mikkelkonyher@gmail.com>
2024-02-19 13:01:44 +0100

    19.02 13.01

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -67,10 +67,39 @@
 //string message = "Hello " + firstName;
 //Console.WriteLine(message);
 
-string firstName = "Bob";
-string greeting = "Hello";
-string message = greeting + " " + firstName + "!";
-Console.WriteLine(message);
+////string firstName = "Bob";
+//string greeting = "Hello";
+//string message = greeting + " " + firstName + "!";
+//Console.WriteLine(message);
+
+////string message = $"Hello {firstName}!";
+////Console.WriteLine(message);
+
+//int version = 11;
+//string updateText = "Update to Windows";
+//string message = $"{updateText} {version}";
+//Console.WriteLine(message);
+
+//int version = 11;
+//string updateText = "Update to Windows";
+//Console.WriteLine($"{updateText} {version}!");
+
+//string projectName = "First-Project";
+//Console.WriteLine($@"C:\Output\{projectName}\data");
+
+string projectName = "ACME";
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+
+Console.WriteLine("View English Ouput: \n\t" + $@"C:\Excercise\{projectName}\data.txt" + "\n");
+
+
+Console.WriteLine(russianMessage + "\n\t" + $@"C:\Excercise\{projectName}\ru-RU\data.txt");
+
+
+
+
+
 
 
 
