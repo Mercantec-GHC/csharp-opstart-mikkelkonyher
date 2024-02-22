0b7730b6af86f5717f46dfb75341e7b90a731915
Mikkel konyher <mikkelkonyher@gmail.com>
2024-02-22 14:17:38 +0100

    4.2 Guided project

--- Dag 3.1 - ConsolApp/Program.cs
+++ Dag 3.1 - ConsolApp/Program.cs
@@ -430,4 +430,32 @@ Console.WriteLine($"Your role '{userInput}' has been accepted.");
 
 
 string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+int stringsCount = myStrings.Length;
+
+string myString = "";
+int periodLocation = 0;
+
+for(int i = 0; i < stringsCount; i++)
+{
+    myString = myStrings[i];
+    periodLocation = myString.IndexOf(".");
+
+    string mySentence;
+
+    while(periodLocation != -1)
+    {
+        mySentence = myString.Remove(periodLocation);
+
+        myString = myString.Substring(periodLocation + 1);
+
+        myString.TrimStart();
+
+        periodLocation = myString.IndexOf(".");
+
+        Console.WriteLine(mySentence);
+    }
+
+    mySentence = myString.Trim();
+    Console.WriteLine(mySentence);
+}
 
